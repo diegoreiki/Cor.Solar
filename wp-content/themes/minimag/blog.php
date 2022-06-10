bf7f40972215f5ea69e10280ea03430cdeee6eb3
Diego Costa Pereira <diegoasp@gmail.com>
2022-06-10 13:08:18 -0300

    development blog page

--- wp-content/themes/minimag/blog.php
+++
@@ -1,9 +0,0 @@
-<?php /* Template Name: Blog */ ?>
-
-<?php
-    $home = get_template_directory_uri();
-    get_header();
-?>
-test
-
-<?php get_footer(); ?>
--- wp-content/themes/minimag/template-parts/siteloader.php
+++
@@ -1,6 +0,0 @@
-<!-- Loader -->
-<div id="site-loader" class="load-complete">
-	<div class="loader">
-		<div class="line-scale"><div></div><div></div><div></div><div></div><div></div></div>
-	</div>
-</div><!-- Loader /- -->
